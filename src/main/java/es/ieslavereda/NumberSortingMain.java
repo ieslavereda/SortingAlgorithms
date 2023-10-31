8b0796b65f73fe715da19b1dbe27b362030e92db
Joaquin Vicente Alonso Saiz <jv.alonso@edu.gva.es>
2023-10-31 12:30:36 +0100

    Initial commit

--- src/main/java/es/ieslavereda/NumberSortingMain.java
+++ src/main/java/es/ieslavereda/NumberSortingMain.java
@@ -6,6 +6,7 @@ import es.ieslavereda.algorithms.iterative.*;
 import static es.ieslavereda.tools.Array.fill;
 
 public class NumberSortingMain {
+
     public static void main(String[] args) {
 
         int[] sizes = new int[]{100000,200000,300000,400000,500000,600000,700000,800000,900000,1000000,1100000,1200000,1300000,1400000,1500000};
--- src/main/java/es/ieslavereda/StringSortingMain.java
+++ src/main/java/es/ieslavereda/StringSortingMain.java
@@ -6,7 +6,6 @@ import static es.ieslavereda.tools.Display.mostrar;
 public class StringSortingMain {
     public static void main(String[] args) {
 
-        //int[] array = new int[]{5,3,78,9,4,2};
         String[] array = new String[]{"coche","moto","zoo","casa","melon"};
 
         sort(array);
